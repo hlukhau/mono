67bcd3a4c758ce7ef3e4e553c6761a3a81174c2f
hlukhau <dzmitry.hlukhau@outlook.com>
2022-03-05 10:07:53 +0300

    commit 01

---
+++ src/test/java/by/psu/vs/mono/MonoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.psu.vs.mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
