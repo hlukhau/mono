5c108871287b4173302710923083cc1561f2a8f1
hlukhau <dzmitry.hlukhau@outlook.com>
2022-03-07 12:09:06 +0300

    Ready (configuration.json)

--- src/main/java/by/psu/vs/mono/controllers/OpenController.java
+++ src/main/java/by/psu/vs/mono/controllers/OpenController.java
@@ -1,18 +1,26 @@
 package by.psu.vs.mono.controllers;
 
+import by.psu.vs.mono.documents.Desktop;
 import by.psu.vs.mono.services.ScreenshotService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/")public class OpenController {
@@ -39,10 +47,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
     }
 
     @GetMapping("/")
-    public String template(Model model) {
+    public String template(Model model) throws IOException {
+        File jsonFile = new File("configuration.json");
+        String json = new String(Files.readAllBytes(jsonFile.toPath()));
+        ArrayList result =
+                new ObjectMapper().readValue(json, ArrayList.class);
+
         String msg = "Welcome to Vladimir Template";
         // adding the attribute(key-value pair)
         model.addAttribute("message", msg);
+        model.addAttribute("desktops", result);
         // returning the view name
         return "index";
     }
